718d6de53e0b6bfb8ef6fdb422e9340ce8a4d028
test-sivani <sivanipratapa@gmail.com>
2024-06-22 16:50:28 +0530

    Update test-fis-et-iamrole.py

--- test-fis-et-iamrole.py
+++ test-fis-et-iamrole.py
@@ -15,7 +15,24 @@ def get_iam_role_policies(role_name):
     attached_policies = iam_client.list_attached_role_policies(RoleName=role_name)
     
     for policy in attached_policies['AttachedPolicies']:
-        aws_managed_policies.append(policy['PolicyName'])
+        policy_arn = policy['PolicyArn']
+        policy_info = iam_client.get_policy(PolicyArn=policy_arn)
+        if policy_info['Policy']['Arn'].startswith('arn:aws:iam::aws:policy/'):
+            # This is an AWS managed policy
+            aws_managed_policies.append(policy_info['Policy']['PolicyName'])
+        else:
+            # This is a customer managed policy
+            policy_version = policy_info['Policy']['DefaultVersionId']
+            policy_document = iam_client.get_policy_version(PolicyArn=policy_arn, VersionId=policy_version)['PolicyVersion']['Document']
+            for statement in policy_document['Statement']:
+                if 'Action' in statement:
+                    actions = statement['Action']
+                    if isinstance(actions, list):
+                        customer_managed_policy_actions.extend(actions)
+                    else:
+                        customer_managed_policy_actions.append(actions)
+                if 'Resource' in statement and statement['Resource'] == '*':
+                    customer_managed_policy_resources_with_star.append(policy_info['Policy']['PolicyName'])
     
     # Get inline policies
     inline_policies = iam_client.list_role_policies(RoleName=role_name)
@@ -34,22 +51,6 @@ def get_iam_role_policies(role_name):
             if 'Resource' in statement and statement['Resource'] == '*':
                 inline_policy_resources_with_star.append(policy_name)
     
-    # Get customer managed policies
-    for policy in attached_policies['AttachedPolicies']:
-        policy_arn = policy['PolicyArn']
-        policy_version = iam_client.get_policy(PolicyArn=policy_arn)['Policy']['DefaultVersionId']
-        policy_document = iam_client.get_policy_version(PolicyArn=policy_arn, VersionId=policy_version)['PolicyVersion']['Document']
-        
-        for statement in policy_document['Statement']:
-            if 'Action' in statement:
-                actions = statement['Action']
-                if isinstance(actions, list):
-                    customer_managed_policy_actions.extend(actions)
-                else:
-                    customer_managed_policy_actions.append(actions)
-            if 'Resource' in statement and statement['Resource'] == '*':
-                customer_managed_policy_resources_with_star.append(policy['PolicyName'])
-    
     return {
         'AWSManagedPolicies': aws_managed_policies,
         'InlinePolicyActions': inline_policy_actions,
@@ -59,12 +60,8 @@ def get_iam_role_policies(role_name):
     }
 
 def lambda_handler(event, context):
-    role_name = event.get('role_name')
-    if not role_name:
-        return {
-            'statusCode': 400,
-            'body': 'role_name is required in the event input'
-        }
+    # Set the role name directly for testing
+    role_name = 'test_sivani'
     
     try:
         policies_info = get_iam_role_policies(role_name)
@@ -78,4 +75,4 @@ def lambda_handler(event, context):
             'body': str(e)
         }
 
-# Note: This lambda_handler function is the entry point for AWS Lambda execution.
+# This lambda_handler function is the entry point for AWS Lambda execution.
